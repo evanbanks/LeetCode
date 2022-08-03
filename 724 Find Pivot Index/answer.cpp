d099e82ecf38e69557c2bff729a68928331ab9a0
Evan Banks <evan.banks@colorado.edu>
2022-08-03 16:56:24 -0600

    solved 724

---
+++ 724 Find Pivot Index/answer.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+class Solution {
+public:
+    int pivotIndex(const std::vector<int>& nums) {
+        int l_sum = 0, r_sum = 0;
+        for (size_t i=1; i<nums.size(); ++i){
+            r_sum += nums[i];
+        }
+        for (size_t i=0; i<nums.size()-1; ++i){
+            if (l_sum == r_sum){
+                return i;
+            }
+            l_sum += nums[i];
+            r_sum -= nums[i+1];
+        }
+        return l_sum == r_sum ? nums.size()-1 : -1;
+    }
+};
