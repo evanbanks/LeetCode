ea11dfba45f21728edff9af7a200ab5f09f387a1
Evan Banks <evan.banks@colorado.edu>
2022-08-09 11:16:00 -0600

    solved 611

--- 976 Largest Perimeter Triangle/answer.cpp
+++ 976 Largest Perimeter Triangle/answer.cpp
@@ -6,7 +6,7 @@ public:
         std::sort(nums.begin(),nums.end());
         for (size_t i=nums.size()-1; i>1; --i){
             if (nums[i-2] + nums[i-1] > nums[i]){
-                return nums[i-2]+nums[i-1]+nums[i];
+                return nums[i-2] + nums[i-1] + nums[i];
             }
         }
         return 0;
