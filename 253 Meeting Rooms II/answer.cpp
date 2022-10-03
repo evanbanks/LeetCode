9247251a5b8e3a0f49bd363b5fa119844736c227
Evan Banks <evan.banks@colorado.edu>
2022-10-03 15:59:20 -0600

    removed unnecessary var

--- 253 Meeting Rooms II/answer.cpp
+++ 253 Meeting Rooms II/answer.cpp
@@ -9,21 +9,15 @@ struct {
 class Solution {
 public:
   int minMeetingRooms(std::vector<std::vector<int>>& in) {
-    int rooms = 1;
-    int free_rooms = 0;
+    size_t rooms = 1;
     std::sort(in.begin(),in.end());
     std::priority_queue q(in.begin(),in.begin()+1,less_comp);
     for (size_t i=1; i<in.size(); ++i){
       while (!q.empty() && in[i][0] >= q.top()[1]){
-        ++free_rooms;
         q.pop();
       }
-      --free_rooms;
-      if (free_rooms < 0){
-        ++free_rooms;
-        ++rooms;
-      }
       q.push(in[i]);
+      rooms = std::max(rooms,q.size());
     }
     return rooms;
   }
