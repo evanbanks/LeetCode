1d6c5a1ea873e69612ef7795e53f58b1f22f0ad0
Evan Banks <evan.banks@colorado.edu>
2022-10-04 15:45:38 -0600

    more concise solution

--- 57 Insert Interval/answer.cpp
+++ 57 Insert Interval/answer.cpp
@@ -2,25 +2,20 @@
 
 class Solution {
 public:
-  std::vector<std::vector<int>> insert(const std::vector<std::vector<int>>& in, const std::vector<int>& newIn) {
+  std::vector<std::vector<int>> insert(const std::vector<std::vector<int>>& in, std::vector<int>& newIn) {
     std::vector<std::vector<int>> mi;
     size_t i = 0;
-    while (i < in.size() && in[i][0] < newIn[0]){
-      mi.push_back(in[i++]);
-    }
-    if (!mi.size() || mi[mi.size()-1][1] < newIn[0]){
-      mi.push_back(newIn);
-    } else {
-      mi[mi.size()-1][1] = std::max(mi[mi.size()-1][1],newIn[1]);
-    }
-    size_t j = mi.size()-1;
     for (; i<in.size(); ++i){
-      if (mi[j][1] < in[i][0]){
+      if (in[i][1] < newIn[0]){
         mi.push_back(in[i]);
-        ++j;
-      } else {
-        mi[j][1] = std::max(mi[j][1], in[i][1]);
-      }
+      } else if (in[i][0] <= newIn[1]){
+        newIn[0] = std::min(newIn[0], in[i][0]);
+        newIn[1] = std::max(newIn[1], in[i][1]);
+      } else break;
+    }
+    mi.push_back(newIn);
+    for (; i<in.size(); ++i){
+      mi.push_back(in[i]);
     }
     return mi;
   }
