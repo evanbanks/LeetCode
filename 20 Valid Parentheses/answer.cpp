d8f6ddd02867c7db1c3a32acbe8fd5ec40c92750
Evan Banks <evan.banks@colorado.edu>
2023-03-05 16:51:25 -0700

    solved 20

--- 20 Valid Parentheses/answer.cpp
+++ 20 Valid Parentheses/answer.cpp
@@ -1,30 +1,25 @@
-#include <stack>
-#include <string>
-
 class Solution {
 public:
-  bool isValid(const std::string& s) {
-    std::stack<char> open;
-    for (size_t i=0; i<s.size(); ++i){
-      if (s[i] == '(' || s[i] == '{' || s[i] == '['){
-        open.push(s[i]);
-      } else if (!open.empty()) {
-        switch (s[i]) { // wish this was a 1 liner but ascii had to ruin it
-          case ')':
-            if (open.top() != '(') return false;
-            break;
-          case '}':
-            if (open.top() != '{') return false;
-            break;
-          case ']':
-            if (open.top() != '[') return false;
-            break;
+    bool isValid(const string& s) {
+        std::stack<char> open;
+        for (size_t i=0; i<s.size(); ++i){
+            switch (s[i]){
+                case ']':
+                    if (!open.size() || open.top() != '[') return false;
+                    break;
+                case ')':
+                    if (!open.size() || open.top() != '(') return false;
+                    break;
+                case '}':
+                    if (!open.size() || open.top() != '{') return false;
+                    break;
+                default:
+                    open.push(s[i]);
+                    continue;
+                    break;
+            }
+            open.pop();
         }
-        open.pop();
-      } else {
-        return false;
-      }
+        return open.empty();
     }
-    return open.empty();
-  }
 };
