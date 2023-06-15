e6e66493db55f6cda0439c2cf6ec0b076968d516
Evan Banks <evan.banks@colorado.edu>
2023-06-15 13:13:24 -0600

    solved 1491

--- 1491 Average Salary Excluding the Minimum and Maximum Salary/answer.cpp
+++ 1491 Average Salary Excluding the Minimum and Maximum Salary/answer.cpp
@@ -1,18 +1,6 @@
-#include <vector>
-
 class Solution {
 public:
-    double average(const std::vector<int>& salary) {
-        double sum = 0;
-        int max = 0;
-        int min = __INT32_MAX__;
-        for (size_t i=0; i<salary.size(); ++i){
-            sum += salary[i];
-            max = std::max(max, salary[i]);
-            min = std::min(min, salary[i]);
-        }
-        sum -= max + min;
-        sum /= (salary.size() - 2);
-        return sum;
+    double average(const vector<int>& salary) {
+        return 1.0*std::accumulate(salary.begin(),salary.end(),0-*std::min_element(salary.begin(),salary.end())-*std::max_element(salary.begin(),salary.end()))/(salary.size()-2);
     }
 };
