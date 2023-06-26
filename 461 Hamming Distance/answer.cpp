2217678bcba4dadf4cb00bbb7c6fa918d6a8dba1
Evan Banks <evan.banks@colorado.edu>
2023-06-26 12:27:31 -0600

    solved 461

---
+++ 461 Hamming Distance/answer.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int ans = 0;
+        x ^= y;
+        while (x){
+            if (x&1) ++ans;
+            x >>= 1;
+        }
+        return ans;
+    }
+};
