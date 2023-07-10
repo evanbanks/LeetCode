28afd4c95bd4bcb71116bd5fb710885e95292f50
Evan Banks <evan.banks@colorado.edu>
2023-07-10 11:37:26 -0600

    solved 1493

--- 1493 Longest Subarray of 1's After Deleting One Element/answer.cpp
+++ 1493 Longest Subarray of 1's After Deleting One Element/answer.cpp
@@ -1,13 +1,11 @@
-#include <vector>
-
 class Solution {
 public:
     int longestSubarray(const std::vector<int>& nums) {
-        int max_length = 0, num_zero = 0;
+        int max_length = 0, last_zero = -1;
         for (int ws=0, we=0; we<nums.size(); ++we){
-            if (!nums[we]) ++num_zero;
-            while (num_zero > 1){
-                if (!nums[ws++]) --num_zero;
+            if (!nums[we]){
+                ws = last_zero + 1;
+                last_zero = we;
             }
             max_length = std::max(max_length, we-ws);
         }
