fc8eb0feb1f7c21df4f4632c81261c856fd99885
Evan Banks <evan.banks@colorado.edu>
2023-12-02 13:38:32 -0500

    solved 34

--- 34 Find First and Last Position of Element in Sorted Array/answer.cpp
+++ 34 Find First and Last Position of Element in Sorted Array/answer.cpp
@@ -1,34 +1,22 @@
-#include <vector>
-
 class Solution {
 public:
-  std::vector<int> searchRange(const std::vector<int>& nums, const int target) {
-    std::vector<int> interval = {-1,-1};
-    int l = 0, r = nums.size()-1, mid;
-    while (l <= r){
-      mid = (l+r)/2;
-      if (nums[mid] == target){
-        interval[0] = mid;
-        r = mid-1;
-      } else if (nums[mid] > target){
-        r = mid-1;
-      } else {
-        l = mid+1;
-      }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> v = {-1,-1};
+        int left = 0, right = nums.size()-1;
+        while (left < right){
+          int mid = (left + right) / 2;
+          if (nums[mid] >= target) right = mid;
+          else left = mid + 1;
+        }
+        if (!nums.size() || nums[left] != target) return v;
+        v[0] = left;
+        left = 0; right = nums.size()-1;
+        while (left < right){
+          int mid = (left + right + 1) / 2;
+          if (nums[mid] <= target) left = mid;
+          else right = mid -1;
+        }
+        v[1] = right;
+        return v;
     }
-    l = 0;
-    r = nums.size()-1;
-    while (l <= r){
-      mid = (l+r)/2;
-      if (nums[mid] == target){
-        interval[1] = mid;
-        l = mid+1;
-      } else if (nums[mid] > target){
-        r = mid-1;
-      } else {
-        l = mid+1;
-      }
-    }
-    return interval;
-  }
 };
